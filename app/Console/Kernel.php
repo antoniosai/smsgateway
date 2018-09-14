367d8e3f2fa0f36534e8a5869997fab069e72152
Antonio Saiful Islam <finallyantonio@gmail.com>
2018-09-14 20:24:47 +0700

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('sms:send')
-            ->dailyAt('15:00');
+            ->dailyAt('19:38');
     }
 
     /**
